8654b6fa8ebb45b0726b52edda84500d6125bf35
Diego Becker <diegoluizbecker@gmail.com>
2020-11-04 09:32:45 -0300

    update

--- listaencadeada.asm
+++ listaencadeada.asm
@@ -4,17 +4,19 @@ vetor:	.word
 msg_opc:	.string "Digite uma opcao"
 quebra_linha:	.string "\n"
 msp_opc_1:	.string "1 - Inserir elemento"
-msp_opc_2:	.string "2 - Remover elemento"
-msp_opc_3:	.string "3 - Consultar elemento"
-msp_opc_4:	.string "4 - Verificar elemento"
+msp_opc_2:	.string "2 - Remover elemento por indice"
+msp_opc_3:	.string "3 - Remover elemento por valor"
+msp_opc_4:	.string "4 - Listar elementos da lista"
 msp_opc_5:	.string "5 - Sair"
 msg_vet:	.string "Vetor: "
+msg_valor_erro: .string "Valor digitado invalido, por favor, insiva novamente um valor valido"
 	.text
 main:
 	
 	j lista_opcoes
 
 lista_opcoes: #faz chamada para listar todos os elementos que podem ser chamados
+	addi t6, zero, 1
 	la a0, msg_opc
 	li a7, 4
 	ecall
@@ -51,12 +53,37 @@ lista_opcoes: #faz chamada para listar todos os elementos que podem ser chamados
 	la a0, quebra_linha
 	li a7, 4
 	ecall
-	li sp, 5
-	add t3, t3, sp
-	ecall
 	li a7, 5 #le valor digitado no teclado e armazena em a0
 	ecall
+	add t3, zero, a0 #move valor do teclado para t3
+	beq t3, t6, insere_elemento
+	addi t6, t6, 1		#gambiarra p/ usar so um registrador e verificar se o numero digitado
+	beq t3, t6, remover_por_indice	#e igual ao ao que esta no reg, toda vez sendo somado
+	addi t6, t6, 1
+	beq t3, t6, remover_por_valor
+	addi t6, t6, 1
+	beq t3, t6, listar_elementos
+	addi t6, t6, 1
+	beq t3, t6, end
+	la a0, msg_valor_erro #caso o valor nao seja da lista informada, 
+	li a7, 4		#ele dá erro e manda inserir novamente
+	ecall
+	la a0, quebra_linha
+	li a7, 4
+	ecall
+	j main
+	
+insere_elemento:
 	j end
+
+remover_por_indice:
+	j end
+	
+remover_por_valor:
+	j end
+
+listar_elementos:
+	j end	
 	
 end:
 	nop
